1ad54b84ca041cce4130bcd257f3e284c34614e7
vamsi <vamsikanakavety144@gmail.com>
2021-08-18 16:13:51 +0530

    addition

--- myapp-aug-2021/src/com/ofss/HelloWorld.java
+++ myapp-aug-2021/src/com/ofss/HelloWorld.java
@@ -1,10 +1,13 @@
 package com.ofss;
 
 public class HelloWorld {
-
+   public static int add(int a, int b) {
+	   return a+b;
+   }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Github");
+System.out.println(add(10,20));
 	}
 
 }
